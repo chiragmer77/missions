ecaa6f23c527767f08ca9a7cc0ffcdfec0378ecc
FGC-Chirag-Mer <chirag.mer@fintechglobal.center>
2023-07-18 23:27:56 +0530

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { LoginComponent } from './modules/authentication/authentication/login/login.component';
+import { ForgotPasswordComponent } from './modules/authentication/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './modules/authentication/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
