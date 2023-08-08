87d65ba209d016f9b6f6d13379d09b1bf0f7adea
chiragmer5077 <chiragbmer5077@gmail.com>
2023-08-08 18:58:18 +0530

    some changes

--- src/app/modules/layout/analytics/analytics.component.ts
+++ src/app/modules/layout/analytics/analytics.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/services/api.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-analytics',
@@ -8,28 +12,69 @@ import Chart from 'chart.js/auto';
 })
 export class AnalyticsComponent {
   myChart: any;
+  hoursSpendByMembersList: any = [];
+  missionStatsData: any = [];
+
+  constructor(
+    private apiService: ApiService,
+    private toaster: ToastrService,
+    private spinner: NgxSpinnerService,
+    private sharedService: SharedService
+  ) { }
 
   ngOnInit() {
-    // Sample data for the chart
-    const data = {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    };
+    this.hoursSpendByMembers();
+    this.missionStats();
+  }
 
-    // Create and render the chart
-    this.myChart = new Chart('myChart', {
-      type: 'bar',
-      data: data,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
+  /** Hours Spent by Members */
+  hoursSpendByMembers() {
+    let params = {
+      From: '2023-07-26',
+      To: '2023-08-08'
+    };
+    this.apiService.getWithParams('Dashboard/hours-member', `From=${params.From}&To=${params.To}`).subscribe((res: any) => {
+      if (res.success === true) {
+        this.hoursSpendByMembersList = res.data;
+
+        const labels = this.hoursSpendByMembersList.map((member: any) => member.fullName); // Array of full names
+        const dataHours = this.hoursSpendByMembersList.map((member: any) => member.totalHours); // Array of total hours
+
+        // Sample data for the chart
+        const data: any = {
+          labels: labels,
+          datasets: [{
+            label: '# of Votes',
+            data: dataHours,
+            borderWidth: 1
+          }]
+        };
+
+        // Create and render the chart
+        this.myChart = new Chart('myChart', {
+          type: 'bar',
+          data: data,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
           }
-        }
+        });
+      }
+    });
+  }
+
+  /** Mission Stats */
+  missionStats() {
+    let params = {
+      From: '2022-07-26',
+      To: '2023-08-08'
+    };
+    this.apiService.getWithParams('Dashboard/stats', `From=${params.From}&To=${params.To}`).subscribe((res: any) => {
+      if (res.success === true) {
+        this.missionStatsData = res.data;
       }
     });
   }
--- src/app/modules/layout/dashboard/dashboard.component.ts
+++ src/app/modules/layout/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/services/api.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +11,101 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  kpiData: any;
+  hoursSpendByMembersList: any = [];
+  hoursMonthList: any = [];
+  chart: any;
 
-
-  constructor(private spinner: NgxSpinnerService) { }
+  constructor(
+    private apiService: ApiService,
+    private toaster: ToastrService,
+    private spinner: NgxSpinnerService,
+    private sharedService: SharedService
+  ) { }
 
 
   ngOnInit() {
-    /** spinner starts on init */
-    this.spinner.show();
+    this.getKpiData();
+    this.hoursSpendByMembers();
+    this.hoursChart();
+    this.createChart();
+  }
 
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 5000);
+  /** Get KPI data */
+  getKpiData() {
+    this.apiService.get('Dashboard/kpi').subscribe((res: any) => {
+      if (res.success === true) {
+        this.kpiData = res;
+      }
+    });
+  }
+
+  /** Hours Spent by Members */
+  hoursSpendByMembers() {
+    let params = {
+      From: '2023-07-26',
+      To: '2023-08-08'
+    };
+    this.apiService.getWithParams('Dashboard/hours-member', `From=${params.From}&To=${params.To}`).subscribe((res: any) => {
+      if (res.success === true) {
+        this.hoursSpendByMembersList = res.data;
+      }
+    });
+  }
+
+  /** Hours Chart */
+  hoursChart() {
+    let params = {
+      From: '2023-07-26',
+      To: '2023-08-08'
+    };
+    this.apiService.getWithParams('Dashboard/hours-month', `From=${params.From}&To=${params.To}`).subscribe((res: any) => {
+      if (res.success === true) {
+        this.hoursMonthList = res.data;
+      }
+    });
+  }
+
+  createChart() {
+    const ctx: any = document.getElementById('linearChart');
+    const monthlyData = [
+      { month: 'Jan', hours: 10 },
+      { month: 'Feb', hours: 20 },
+      { month: 'Mar', hours: 30 },
+      { month: 'Apr', hours: 40 },
+      { month: 'May', hours: 50 },
+      { month: 'Jun', hours: 60 },
+      { month: 'Jul', hours: 70 },
+      { month: 'Aug', hours: 80 },
+      { month: 'Sep', hours: 90 },
+      { month: 'Oct', hours: 100 },
+      { month: 'Nov', hours: 110 },
+      { month: 'Dec', hours: 120 },
+      // ...and so on for other months
+    ];
+
+    const data = monthlyData.map(entry => entry.hours);
+    const labels = monthlyData.map(entry => entry.month);
+
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Hours Spent',
+          data: data,
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)', // Background color below the line
+          fill: true // This fills the area below the line
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 }
